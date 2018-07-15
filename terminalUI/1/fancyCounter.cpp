79250deea573d64dd8d6aaf317901cb280101b4a
AlexS <sobolevalex.dom@gmail.com>
2018-07-15 13:46:48 +0300

    fancy counter minor changes

--- terminalUI/1/fancyCounter.cpp
+++ terminalUI/1/fancyCounter.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <chrono>
 #include <memory>
+#include <iostream>
 
 #include "terminal.hpp"
 #include "style.hpp"
@@ -22,26 +23,39 @@ auto renderToTerm = [](auto const& vt, unsigned const w, Component const& c) {
 
 int main() {
 
+  std::string tr = "a";
+
+  std::cout << std::endl;
   using namespace std::chrono_literals;
   using namespace std::string_literals;
 
-  VirtualTerminal vt;
-
-  auto superProgressBar = [](auto x, auto y, auto z) -> FlowLayout<> {
-    return {
-      Text("3 segment progressbar: "),
-      FlowLayout<>{
-        MaxWidth(20, Progress(x)),
-        MaxWidth(20, Progress(y)),
-        MaxWidth(20, Progress(z))
-      }
-    };
-  };
-
-  for (int i = 0; i < 101; ++i) {
-    vt = renderToTerm(vt, 80, superProgressBar(0.01 * i, 0.02 * i, 0.03 * i));
+  for(size_t i = 0; i < 30; i++)
+  {
+    tr += "a";
+    std::cout << clearLines(1)<< tr << std:: endl;
     std::this_thread::sleep_for(200ms);
+    //tr += "a";
   }
+  
+
+
+  // VirtualTerminal vt;
+
+  // auto superProgressBar = [](auto x, auto y, auto z) -> FlowLayout<> {
+  //   return {
+  //     Text("3 segment progressbar: "),
+  //     FlowLayout<>{
+  //       MaxWidth(20, Progress(x)),
+  //       MaxWidth(20, Progress(y)),
+  //       MaxWidth(20, Progress(z))
+  //     }
+  //   };
+  // };
+
+  // for (int i = 0; i < 101; ++i) {
+  //   vt = renderToTerm(vt, 80, superProgressBar(0.01 * i, 0.02 * i, 0.03 * i));
+  //   std::this_thread::sleep_for(200ms);
+  // }
 
   return 0;
 }
